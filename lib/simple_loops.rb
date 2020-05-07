5a9e6df876e092690033e7394b34f1bc39779d7b
malcolm staso <mstaso1@gmail.com>
2020-05-07 18:42:39 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,15 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+5.times do |message|
+  puts "Hello World."
+end
+end
+
+def loop_message_n_times(message, number)
+  count = 0
+  while count < #{number} do
+    puts "#{message}."
+    count += 1
+  end
+end
